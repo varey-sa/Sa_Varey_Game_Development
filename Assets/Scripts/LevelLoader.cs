a5aea2807cb567c42a3eaae08f8c55d0355d0c6b
Varey <varey@mango-byte.com>
2019-10-13 20:29:56 +0700

    week1 + week 2

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 4;
+    int currentSenceIndex;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        currentSenceIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSenceIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSenceIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
